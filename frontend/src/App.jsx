bec24f328a2a02910aa55d012ecdfccaa3cb47f0
BestarDev <superstar9772@outlook.com>
2024-06-25 21:29:50 +0700

    Adding Footer Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { Container } from "react-bootstrap"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <div>
       <Header />
-      <main className="py-3">
+      <main className="py-3" style={{minHeight: '80vh'}}>
         <Container>
           Welcome Proshop
         </Container>
       </main>
+      <Footer />
     </div>
   )
 }
