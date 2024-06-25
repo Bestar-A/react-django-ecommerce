7e3f38076168317fec647d578c6e6d3c1732b081
BestarDev <superstar9772@outlook.com>
2024-06-25 22:34:17 +0700

    Implementing React Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,23 @@ import { Container } from "react-bootstrap"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import HomeScreen from "./screens/HomeScreen"
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import ProductScreen from "./screens/ProductScreen"
 
 const App = () => {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3" style={{minHeight: '80vh'}}>
         <Container>
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
