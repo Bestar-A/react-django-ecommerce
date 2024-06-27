20d96008c121704465baf5322f00b1c93236f142
BestarDev <superstar9772@outlook.com>
2024-06-27 17:54:19 +0700

    JWT Customization

--- backend/base/views.py
+++ backend/base/views.py
@@ -10,12 +10,13 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    @classmethod
-    def get_token(cls, user):
-        token = super().get_token(user)
-        token['username'] = user.username
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        
+        data['username'] = self.user.username
+        data['email'] = self.user.email
 
-        return token
+        return data
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
