50488aeda9d93dfd9151f72c4d4284a94694aff0
BestarDev <superstar9772@outlook.com>
2024-06-28 08:25:54 +0700

    Header Modify & Logout Handler

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,41 +1,59 @@
 import { BiUserCircle } from "react-icons/bi"; 
 import { AiOutlineShoppingCart } from "react-icons/ai"; 
-import  { Container, Nav, Navbar } from 'react-bootstrap'
+import  { Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../store/slices/authSlice";
 
 const Header = () => {
-  return (
-    <header>
-        <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-            <Container>
-                <LinkContainer to='/'>
-                    <Navbar.Brand>Proshop</Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls='navbar-menus'/>
-                <Navbar.Collapse id="navbar-menus">
-                    <Nav className='ms-auto'>
-                        <LinkContainer to='/cart'>
-                            <Nav.Link>
-                                <div className="d-flex align-items-center gap-1 text-white">
-                                    <AiOutlineShoppingCart />
-                                    Cart
-                                </div>
-                            </Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to='/login'>
-                            <Nav.Link>
-                                <div className="d-flex align-items-center gap-1 text-white">
-                                    <BiUserCircle />
-                                    Login
-                                </div>
-                            </Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    </header>
-  )
+    const { userInfo } = useSelector(state => state.auth)
+    const dispatch = useDispatch()
+
+    const logoutHandler = () => {
+        dispatch(logout())
+    }
+
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                <Container>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Proshop</Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls='navbar-menus'/>
+                    <Navbar.Collapse id="navbar-menus">
+                        <Nav className='ms-auto'>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link>
+                                    <div className="d-flex align-items-center gap-1 text-white">
+                                        <AiOutlineShoppingCart />
+                                        Cart
+                                    </div>
+                                </Nav.Link>
+                            </LinkContainer>
+                            {userInfo ? (
+                                <NavDropdown title={userInfo.name} id='username'>
+                                    <LinkContainer to='/profile'>
+                                        <NavDropdown.Item>Profile</NavDropdown.Item>
+                                    </LinkContainer>
+                                    <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <LinkContainer to='/login'>
+                                    <Nav.Link>
+                                        <div className="d-flex align-items-center gap-1 text-white">
+                                            <BiUserCircle />
+                                            Login
+                                        </div>
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
